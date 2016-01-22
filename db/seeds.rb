74b6c83281c9fea4f9169f240a8036dd82ed6f5c
Dreamtaker34 <dreamtaker@gmail.com>
2016-01-22 13:31:52 -0500

    Blocitoff - User Story  - Seeding Data - complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+include Faker
+
+4.times do
+  User.create!(
+  username: Faker::Internet.user_name,
+  email: Faker::Internet.safe_email,
+  password: Faker::Internet.password,
+  )
+end
+
+User.create!(
+  username: "test",
+  email: "test@example.com",
+  password: "helloworld"
+  )
+
+
+users = User.all
+
+puts "#{users.sample[:id]}"
+
+50.times do
+  Item.create!(
+  name: Faker::Lorem.sentence(4),
+  user_id: users.sample[:id],
+  )
+end
+items = Item.all
+
+@user = users.sample
+
+puts "Created #{User.count} users."
+puts "Created #{Item.count} To-Do's."
+puts "#{@user.username} has to do #{@user.items.first.name}"
